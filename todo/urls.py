5625798085d58c06b23a14958463ba9a46aba5b8
pxleblank <ivanolegovi@mail.ru>
2024-07-31 19:05:35 +0700

    little update with html (i sometimes use html-constructor)

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.current_datetime)
+    path('', views.todolist, name='todolist'),
+    path('cabinet/', views.cabinet, name='cabinet')
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def current_datetime(request):
-    html = "<html><body><h1>TODO LIST:</h1></body></html>"
-    return HttpResponse(html)
+def todolist(request):
+    return render(request, 'todo/htmltodolist.html',
+
+                  )
+
+def cabinet(request):
+    return render(request, 'todo/cabinet.html',
+
+                  )
