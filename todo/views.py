632397e0ff3a0e0254b5f62ba63764e1d0c0a059
pxleblank <ivanolegovi@mail.ru>
2024-08-02 20:07:02 +0700

    make placeholders and template tags

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 def todolist(request):
     return render(request, 'todo/html_todo_list.html',
                   context={
+                      'title': 'Todo List',
                       'user': 'blank.',
                       'is_tasks': False,
                   }
--- user/views.py
+++ user/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def cabinet(request):
     return render(request, 'user/html_user_cabinet.html',
-
+                  context={
+                      'title': 'Cabinet'
+                  }
                   )
