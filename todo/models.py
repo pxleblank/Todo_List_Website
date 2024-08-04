d63e7b9fcbc46f12ea6bb3c9903be3376d85189c
pxleblank <ivanolegovi@mail.ru>
2024-08-04 22:33:12 +0700

    make __str__ and add user

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,6 @@ class Todolist(models.Model):
     user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
     task = models.TextField(null=False, blank=False)
     state = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.task
--- user/models.py
+++ user/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=64, null=False, blank=False)
+    username = models.CharField(max_length=64, null=False, blank=False, unique=True)
     email = models.EmailField(null=False, blank=False)
     password = models.CharField(max_length=64, null=False, blank=False)
+
+    def __str__(self):
+        return self.username
