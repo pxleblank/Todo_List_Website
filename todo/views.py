da4d77aed74a45fe96b770fb7670934d6ba6d57e
pxleblank <ivanolegovi@mail.ru>
2024-08-09 18:37:07 +0700

    create login form

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from todo.models import Todolist
 
@@ -12,17 +13,17 @@ def todolist(request):
             task_content = request.POST.get('post_contents')
             if task_content:
                 Todolist.objects.create(user=request.user, task=task_content)
-                return redirect('todolist')
+                return redirect(reverse('todolist'))
         elif 'delete_task' in request.POST:
             task_id = request.POST.get('delete_task')
             Todolist.objects.filter(user=request.user, id=task_id).delete()
-            return redirect('todolist')
+            return redirect(reverse('todolist'))
         elif 'change_state' in request.POST:
             task_id = request.POST.get('change_state')
             task = Todolist.objects.get(user=request.user, id=task_id)
             task.state = 'completed' if task.state == 'in progress' else 'in progress'
             task.save()
-            return redirect('todolist')
+            return redirect(reverse('todolist'))
 
 
     return render(request, 'todo/html_todo_list.html',
--- user/views.py
+++ user/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import auth
+from django.urls import reverse
 
-# from user.models import User
+from user.models import User
+from user.forms import UserLoginForm
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect(reverse('todolist'))
+    else:
+        form = UserLoginForm()
+
+
     return render(request, 'user/login_form.html',
                   context={
-                        'title': 'Login'
+                      'title': 'Login',
+
+                      'form': form
                   }
                   )
 
@@ -16,7 +34,7 @@ def login(request):
 def register(request):
     return render(request, 'user/registry_form.html',
                   context={
-                        'title': 'Register'
+                      'title': 'Register'
                   }
                   )
 
